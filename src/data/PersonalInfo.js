8b08aecea0b0d593597875372d04b2607ab294cd
Guangning Li <gngnggnn1@hotmail.com>
2025-09-09 20:10:04 -0700

    0909

--- src/data/PersonalInfo.js
+++ src/data/PersonalInfo.js
@@ -1,8 +1,8 @@
 export const personalInfo = {
   name: "Guangning Li",
-  tagline: "IT & Cybersecurity Specialist | Master of Information Security Student @ UVic | EIT",
+  tagline: "IT & Cybersecurity Specialist | Master of Information Security | EIT",
   bio: [ // Use an array for paragraphs
-    "Hello! I’m a grad student in Information Security with a passion for building secure, reliable, and practical digital systems. I enjoy working with modern tech and finding smart solutions to real-world problems.",
+    "Hello! I hold a Master's Degree in Information Security with a passion for building secure, reliable, and practical digital systems. I enjoy working with modern tech and finding smart solutions to real-world problems.",
     "I got into tech out of curiosity—wanting to understand how systems work and how to make them more secure. Since then, I’ve been focused on growing my skills in cybersecurity, cloud infrastructure, and software development."
   ],
   linkedin: "https://www.linkedin.com/in/guangning-li/",
